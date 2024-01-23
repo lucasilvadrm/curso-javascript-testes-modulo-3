d9cf16ba3f0a10f31c858df0358ae7241f7bdabf
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-23 11:26:12 -0300

    test: adicionando teste para quando a busca for vazia

--- components/search.unit.spec.js
+++ components/search.unit.spec.js
@@ -6,6 +6,12 @@ import userEvent from '@testing-library/user-event';
 const doSearch = jest.fn();
 
 describe('Search', () => {
+
+  // realizando o reset dos mocks antes de cada teste
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
   it('should render a form', () => {
     render(<Search />);
 
@@ -13,7 +19,7 @@ describe('Search', () => {
   });
 
   it('should render a input equals search', () => {
-    render(<Search doSearch={doSearch}/>);
+    render(<Search doSearch={doSearch} />);
 
     // verificando type do input
     expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
@@ -30,21 +36,34 @@ describe('Search', () => {
 
     // espera que o método doSearch seja chamado ao menos uma vez
     expect(doSearch).toHaveBeenCalledTimes(1);
-
   });
 
   it('should call props.doSearch() with the user input', async () => {
     render(<Search doSearch={doSearch} />);
 
     //criando referência ao form
-    const inputText = 'some text here'
+    const inputText = 'some text here';
     const form = screen.getByRole('form');
     const input = screen.getByRole('searchbox');
 
     await userEvent.type(input, inputText);
     await fireEvent.submit(form);
 
-    expect(doSearch).toHaveBeenCalledWith(inputText)
+    expect(doSearch).toHaveBeenCalledWith(inputText);
+  });
 
+  fit('should call doSearch when search input is cleared', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    //criando referência ao form
+    const inputText = 'some text here';
+    const input = screen.getByRole('searchbox');
+
+    // adicionando um texto no input
+    await userEvent.type(input, inputText);
+    await userEvent.clear(input);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
+    expect(doSearch).toHaveBeenCalledWith('');
   });
 });
