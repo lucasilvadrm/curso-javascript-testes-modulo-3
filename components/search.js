fef122bb3d396e53472bab4b378c43ef2e1e9c83
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-23 10:52:42 -0300

    feat: implementando busca

--- components/search.js
+++ components/search.js
@@ -4,8 +4,13 @@ export default function Search({ doSearch }) {
 
   const [term, setTerm] = useState('');
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    doSearch(term)
+  }
+
   return (
-    <form onSubmit={() => doSearch(term)} name="search-form" className="relative mt-6 max-w-lg mx-auto">
+    <form onSubmit={submitHandler} name="search-form" className="relative mt-6 max-w-lg mx-auto">
       <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
         <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
           <path
