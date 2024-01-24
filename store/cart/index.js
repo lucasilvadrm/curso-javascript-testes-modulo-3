f2228f242dcef9bee86c72f88d502a46935d1460
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-24 16:00:43 -0300

    feat: estado global da aplicação utilizando zustand

--- store/cart/index.js
+++ store/cart/index.js
@@ -1,42 +1,52 @@
 import create from 'zustand';
-import produce, { original } from 'immer';
+import produce from 'immer';
+
+const initialState = {
+  open: false,
+  products: [],
+};
 
 export const useCartStore = create(set => {
   const setState = fn => set(produce(fn));
 
-  const initialState = {
-    open: false,
-    products: [],
-  };
-
   return {
-    state: {
-      ...initialState,
-    },
+    state: { ...initialState },
     actions: {
-      toggle: () =>
+      toggle() {
         setState(({ state }) => {
           state.open = !state.open;
-        }),
-      add: product =>
+        });
+      },
+      reset() {
+        setState(store => {
+          store.state = initialState;
+        });
+      },
+      add(product) {
         setState(({ state }) => {
-          if (!original(state.products).includes(product)) {
+          const doesntExist = !state.products.find(({ id }) => id === product.id);
+          if (doesntExist) {
             state.products.push(product);
+            state.open = true;
           }
-          state.open = true;
-        }),
-      remove: product =>
+        });
+      },
+      remove(product) {
         setState(({ state }) => {
-          const index = original(state.products).indexOf(product);
-          if (index > -1) {
-            state.products.splice(index, 1);
+          const exists = !!state.products.find(({ id }) => id === product.id);
+
+          if (exists) {
+            state.products = state.products.filter(({ id }) => {
+              return id !== product.id;
+            });
           }
-        }),
-      removeAll: () =>
+        });
+      },
+      removeAll() {
         setState(({ state }) => {
           state.products = [];
-        }),
-      reset: () => set({ state: { ...initialState } }),
+        });
+      },
     },
   };
 });
