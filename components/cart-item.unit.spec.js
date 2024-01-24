2c7ea48ca37b4c66b2b344cdeabadd1a75e5a52e
Lucas Silva <lucassilva.uern@gmail.com>
2024-01-24 16:20:49 -0300

    test: adicionando teste para verificar se a função de remove é chamada ao clicar no botão

--- components/cart-item.unit.spec.js
+++ components/cart-item.unit.spec.js
@@ -1,5 +1,11 @@
 import CartItem from './cart-item';
 import { screen, render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useCartStore } from '../store/cart';
+import { renderHook } from '@testing-library/react-hooks';
+import { setAutoFreeze } from 'immer';
+
+setAutoFreeze(false);
 
 const product = {
   title: 'Relógio',
@@ -74,5 +80,20 @@ describe('CartItem', () => {
     await fireEvent.click(buttonDecrease);
 
     expect(quantity.textContent).toBe('0');
+  });
+
+  it('should call remove() when remove button is clicked', async () => {  
+    const result = renderHook(() => useCartStore()).result;
+  
+    const spy = jest.spyOn(result.current.actions, 'remove');
+
+    renderCartItem();
+
+    const button = screen.getByRole('button', { name: /remove/i });
+
+    await userEvent.click(button);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(product);
   });
 });
